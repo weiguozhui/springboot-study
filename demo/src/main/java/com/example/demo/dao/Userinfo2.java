90baf33e74dc3e86a62cf6b76dd8ca8215803d29
weiguozhui <1254548084@qq.com>
2020-12-10 01:57:23 +0800

    idea的database的使用，含有客户端，实体类生成等，so strong！

---
+++ demo/src/main/java/com/example/demo/dao/Userinfo2.java
@@ -0,0 +1,37 @@
+package com.example.demo.dao;
+
+
+public class Userinfo2 {
+
+  private long id;
+  private String userName;
+  private String userPwd;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+
+  public String getUserPwd() {
+    return userPwd;
+  }
+
+  public void setUserPwd(String userPwd) {
+    this.userPwd = userPwd;
+  }
+
+}
