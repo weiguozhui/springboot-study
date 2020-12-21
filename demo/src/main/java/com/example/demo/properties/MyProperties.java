7b497a444c0b6239b238f9d0f73da27f16535978
weiguozhui <1254548084@qq.com>
2020-12-22 00:25:10 +0800

    spring boot的@Value获取配置值使用，及jsr303数据校验的使用

--- demo/src/main/java/com/example/demo/properties/MyProperties.java
+++ demo/src/main/java/com/example/demo/properties/MyProperties.java
@@ -2,19 +2,34 @@ package com.example.demo.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+
 // 定义为自定义配置项映射类
-@ConfigurationProperties(prefix = "myproperties")
+//@ConfigurationProperties(prefix = "myproperties")
 // 注入到容器中
 @Component
 // get和set方法
 @Getter
 @Setter
+//@Validated
 public class MyProperties {
     // 定义对应的属性
+    @Value("aaa")
     private String aaa;
+    @Value("${myproperties.bbb}")
     private String bbb;
     private String ccc;
     private String ddd;
+    @Value("true")
+    private boolean flag;
+    @Value("#{2*3}")
+    private int age;
+
+//    @Email
+    private String email;
 }
