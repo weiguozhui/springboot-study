a393b9676cc56f3bf6d6bdde376e240018947da1
weiguozhui <1254548084@qq.com>
2021-01-14 00:15:54 +0800

    spring boot 日志框架slf4j+logback,日志配置项

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -20,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan(basePackages = "com.example.demo.mapper")
 public class DemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
+		logger.error("===============logback============");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
