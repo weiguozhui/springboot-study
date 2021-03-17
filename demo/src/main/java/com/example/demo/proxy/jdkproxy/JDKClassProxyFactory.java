1bda274c22ed614b3530cced419ed56dc0f44f30
weiguozhui <1254548084@qq.com>
2021-03-18 00:16:03 +0800

    动态代理

--- demo/src/main/java/com/example/demo/proxy/jdkproxy/JDKClassProxyFactory.java
+++ demo/src/main/java/com/example/demo/proxy/jdkproxy/JDKClassProxyFactory.java
@@ -16,5 +16,6 @@ public class JDKClassProxyFactory implements JDKClass{
         System.out.println("before say hello");
         jdkClass.proxyMethod();
         System.out.println("after say hello");
+
     }
 }
